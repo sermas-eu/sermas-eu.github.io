06f13a38d24cb3f1c03eccab1e0b1009d995c395
lcapra <luca.capra@spindox.it>
2024-03-27 11:08:43 +0100

    update config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,9 +15,11 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'sermas-eu', // Usually your GitHub org/user name.
+  projectName: 'sermas-eu.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
+  
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
